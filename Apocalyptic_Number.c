0b83a5fbbb9a0b980f343be02cd180837ee92873
RishabhSinghal04 <rishabhsinghal2302@gmail.com>
2022-08-31 19:55:13 +0530

    added a new file

---
+++ Apocalyptic_Number.c
@@ -0,0 +1,24 @@
+// A number such that 2^n contains '666' as a substring
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <string.h>
+
+void main()
+{
+    int num;
+    char str[100];
+
+    printf("Enter a number as power of 2 (2^n): ");
+    scanf("%d", &num);
+
+    snprintf(str, 100, "%f", pow(2, num));
+    printf("\n%s\n", str);
+
+    if (strstr(str, "666"))
+        printf("Apocalyptic Number");
+    else
+        printf("Not an Apocalyptic Number");
+
+    getch();
+}
